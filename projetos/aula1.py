307554a369cbea55501bb92a1a3e3f6f9207dd6f
Alberto Michael <albertomichael582@gmail.com>
2026-08-03 20:07:06 +0100

    conceitos de listas

--- projetos/aula1.py
+++ projetos/aula1.py
@@ -1,1 +1,3 @@
-print('Hello, World!')
+menssage = 'olá mundo, irei dominar o python'
+print(menssage.upper())
+#comentarios
---
+++ projetos/listas#1.py
@@ -0,0 +1,3 @@
+bicicletas = ['trek','cannondale','redline']
+print(f'a minha primeira bicicleta foi um {bicicletas[1].title()}')
+
